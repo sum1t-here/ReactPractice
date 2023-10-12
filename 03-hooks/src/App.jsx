a2ecf81f10a905e701da14abf909d498b141f493
sumit <mazumdarsumit3@gmail.com>
2023-10-12 22:50:29 +0530

    React hook and it useState use case

---
+++ 03-hooks/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  //      variable, function
+  let [counter, setCounter] = useState(15);
+
+  // let counter = 15;
+
+  const addValue = () => {
+    console.log('Clicked', counter);
+    // setCounter(counter + 1);
+
+    counter = counter + 1;
+    setCounter(counter);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>Hi, this is a counter project</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+
+      <br />
+      <br />
+
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
