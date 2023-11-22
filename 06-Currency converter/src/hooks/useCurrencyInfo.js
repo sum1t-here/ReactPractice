b3c257b40e876898bbddc4a681e598d708de59b4
sumit <mazumdarsumit3@gmail.com>
2023-11-22 12:57:57 +0530

    update custom hook

--- 06-Currency converter/src/hooks/useCurrencyInfo.js
+++ 06-Currency converter/src/hooks/useCurrencyInfo.js
@@ -6,7 +6,7 @@ function useCurrencyInfo(currency) {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
-      .then((res) => res.json) // response coverted to json
+      .then((res) => res.json()) // response coverted to json
 
       // if data is hold in regular variable it is not updated in ui so useState hook is state, it contains an empty object in sense if there is no data empty object is retrieved so there is no crash of app
       .then((res) => setData(res[currency]));
