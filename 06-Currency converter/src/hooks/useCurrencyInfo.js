6c09555b5ad8231c21a09b8a3ce2f9724bbc1418
sumit <mazumdarsumit3@gmail.com>
2023-11-22 12:55:28 +0530

    create custom hook useCurrencyInfo.js

---
+++ 06-Currency converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json) // response coverted to json
+
+      // if data is hold in regular variable it is not updated in ui so useState hook is state, it contains an empty object in sense if there is no data empty object is retrieved so there is no crash of app
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
