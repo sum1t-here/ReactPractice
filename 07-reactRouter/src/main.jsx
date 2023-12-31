9b40278da04ebcbf5970df4f85037578f8b7fe6c
sumit <mazumdarsumit3@gmail.com>
2023-12-31 20:55:08 +0530

    React router practise

---
+++ 07-reactRouter/src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from '../components/Home/Home.jsx';
+import About from '../components/About/About.jsx';
+import Contacts from '../components/Contacts/Contacts.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contacts',
+        element: <Contacts />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
