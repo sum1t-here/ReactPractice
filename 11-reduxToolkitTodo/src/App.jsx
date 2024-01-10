d0beea5adb875157b95a63152fccc4618d7ef08e
sumit <mazumdarsumit3@gmail.com>
2024-01-10 12:36:44 +0530

    Add: components

--- 11-reduxToolkitTodo/src/App.jsx
+++ 11-reduxToolkitTodo/src/App.jsx
@@ -1,7 +1,12 @@
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <AddTodo />
+      <Todos />
     </div>
   );
 }
