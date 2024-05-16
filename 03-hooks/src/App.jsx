b25522516687f9119b6ceee9bc505da457c0ec93
sumit <mazumdarsumit3@gmail.com>
2024-05-16 21:48:13 +0530

    set maxLimit to counter

--- 03-hooks/src/App.jsx
+++ 03-hooks/src/App.jsx
@@ -3,23 +3,16 @@ import './App.css';
 
 function App() {
   //      variable, function
-  let [counter, setCounter] = useState(15);
+  let [counter, setCounter] = useState(0);
 
   // let counter = 15;
+  const maxLimit = 5;
 
   const addValue = () => {
-    console.log('Clicked', counter);
-    // setCounter(counter + 1);
-
-    counter = counter + 1;
-
-    setCounter((prevCounter) => prevCounter + 1);
-    setCounter((prevCounter) => prevCounter + 1);
-    setCounter((prevCounter) => prevCounter + 1);
-    setCounter((prevCounter) => prevCounter + 1);
-    // only updated once not 3 times
-    // in useState updates are set in batches due to fiber
-    // so using counter only with not give the desired result
+    if (counter < maxLimit) {
+      setCounter(counter + 1);
+    }
+    console.log('MAX reached');
   };
 
   const removeValue = () => {
@@ -27,7 +20,7 @@ function App() {
   };
   return (
     <>
-      <h1>Hi, this is a counter project</h1>
+      <h1>Counter within limits</h1>
       <h2>Counter value: {counter}</h2>
       {/* reference of addValue is passed */}
       <button onClick={addValue}>Add Value</button>
